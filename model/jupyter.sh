deacda01d5dae5674d432110e2f58337e45d97ef
Andrei Rukavina <rukavina.andrei@gmail.com>
2018-05-12 17:27:03 -0400

    Removing old models & web

--- model/jupyter.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-echo "Activating python venv: [model-ml]."
-source ~/python-venv/ml-model/bin/activate
-
-ipython kernel install --user --name=baking-lyrics
-
-jupyer notebook
--- web/pip_freeze.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Activating python venv [bl-web]."
-source ~/python-venv/bl-web/bin/activate
-echo "Freezing pip (as is) now."
-pip freeze > requirements.txt
