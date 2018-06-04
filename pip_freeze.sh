e394e7fc53fbc7d1d06fd18a9add28c863495280
arukavina <rukavina.andrei@gmnail.com>
2018-06-04 17:05:57 -0300

    Hardcoding freeze example.

--- pip_freeze.sh
+++ pip_freeze.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Activating python venv: [baking-lyrics]."
-source ~/python-venv/baking-lyrics/bin/activate
+source ~/venv/baking-lyrics/bin/activate
 echo "Freezing pip (as is) now."
 pip freeze > requirements/prd.txt
