fbf4918d2eaef686967f2d8c66c5cdfc22cd4707
arukavina <rukavina.andrei@gmnail.com>
2018-10-14 20:44:51 -0300

    Prd config update

--- config/production.py
+++ config/production.py
@@ -4,3 +4,18 @@ TESTING = False
 PROPAGATE_EXCEPTIONS = None
 PRESERVE_CONTEXT_ON_EXCEPTION = None
 ENV = 'production'
+
+DATA_FILE_PATH = r'/mnt/s3/resources/songdata.csv'
+LYRICS_LSTM_SEED_FILE_PATH = r'/mnt/s3/resources/songdata.csv'
+
+# Log
+LOG_DIR = r'/mnt/s3/logs/'
+LOG_LEVEL = 1
+
+# Lyrics - LSTM
+LYRICS_LSTM_MODEL_FILE_PATH = r'/mnt/s3/resources/generator_v1.yaml'
+LYRICS_LSTM_WEIGHTS_FILE_PATH = r'/mnt/s3/resources/generator_v1_weights.h5'
+
+# Title - LSTM
+TITLE_LSTM_MODEL_FILE_PATH = '/mnt/s3/resources/models/titler_v1.yaml'
+TITLE_LSTM_TOKENIZER_FILE_PATH = '/mnt/s3/resources/models/titler_v1_tokenizer.pickle'
