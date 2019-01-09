d1ccef3b831b11e13f523a2c0d8cc9e5588318b6
arukavina <rukavina.andrei@gmail.com>
2019-01-09 02:00:33 -0300

    Fixing PRD Configuration and DB refresh()

--- config/testing.py
+++ config/testing.py
@@ -4,4 +4,4 @@ TESTING = True
 PROPAGATE_EXCEPTIONS = None
 PRESERVE_CONTEXT_ON_EXCEPTION = None
 ENV = 'testing'
-DATA_FILE_PATH = r'baking/resources/lyrics.csv'
+DATA_FILE_PATH = r'baking/resources/songdata_4_tests.csv'
